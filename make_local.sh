ce0dd4d0771d03fc0b36349f6a4ef7ea125946df
pbmanis <pmanis@med.unc.edu>
2020-09-20 21:41:06 -0400

    update environment builder

--- make_local.sh
+++ make_local.sh
@@ -1,16 +1,16 @@
 ENVNAME="ephys_venv"
-python3 -m venv $ENVNAME
+python3.7 -m venv $ENVNAME
 source $ENVNAME/bin/activate
-pip install --upgrade pip  # be sure pip is up to date in the new env.
-pip install wheel  # seems to be missing (note singular)
-pip install cython
+pip3 install --upgrade pip  # be sure pip is up to date in the new env.
+pip3 install wheel  # seems to be missing (note singular)
+pip3 install cython
 # # if requirements.txt is not present, create:
 # # pip install pipreqs
 # # pipreqs
 #
 # #Then:
 #
-pip install -r requirements_local.txt
+pip3 install -r requirements_local.txt
 source $ENVNAME/bin/activate
-
+python3.7 --version
 python setup.py develop
