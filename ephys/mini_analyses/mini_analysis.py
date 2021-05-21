fixes for ank2 project, and fix averaging/fitting when events run off end of trace in methods_common

ephys/mini_analyses/mini_analysis.py
@@ -88,6 +88,8 @@ class MiniAnalysis:
         self.datasource = dataplan.datasource
         self.basedatadir = dataplan.datadir
         self.shortdir = dataplan.shortdir
+        self.dataset = dataplan.dataset
+        self.outputpath = dataplan.outputpath
         self.dataplan_params = dataplan.dataplan_params
         self.dataplan_data = dataplan.data
         self.min_time = dataplan.min_time
@@ -166,6 +168,7 @@ class MiniAnalysis:
         -------
         Nothing
         """
+        print('analyze_all')
         acqr = EP.acq4read.Acq4Read(
             dataname=self.clamp_name
         )  # creates a new instance every time - probably should just create one.
@@ -185,20 +188,24 @@ class MiniAnalysis:
                 # if index >= 0:
                 #     break
         if not check:
-            fout = f"summarydata_{str(self.shortdir):s}_{str(self.filterstring):s}_{mode:s}.p"
+            print("output file: ", self.shortdir, self.dataset, self.filterstring, mode)
+            ofile = Path(self.outputpath, f"{self.dataset:s}_{str(self.filterstring):s}_{mode:s}.p")
+            fout = str(ofile)
+            print("outfile: ", ofile)
             fh = open(fout, "wb")
             pickle.dump(summarydata, fh)
             fh.close()
         else:
             print("All files found (an exception would be raised if one was not found)")
 
-    def build_summary_dict(self, genotype:Union[str, None] = None, mouse:Union[str, int, None]=None):
+    def build_summary_dict(self, genotype:Union[str, None] = None, eyfp:str = "ND", mouse:Union[str, int, None]=None):
         self.cell_summary = {
             "intervals": [],
             "amplitudes": [],
             "protocols": [],
             "eventcounts": [],
             "genotype": genotype,
+            "EYFP": eyfp,
             "mouse": mouse,
             "amplitude_midpoint": 0.0,
             "holding": [],
@@ -254,6 +261,7 @@ class MiniAnalysis:
         -------
             cell summary dictionary for the 'mouse' entry.
         """
+        print('analyze one cell')
         if arreader is None:
             acqr = EP.acq4read.Acq4Read(
                 dataname=self.clamp_name
@@ -277,7 +285,8 @@ class MiniAnalysis:
             self.sign = int(self.dataplan_data["sign"])
 
         print("\nMouse: ", mouse)
-        self.build_summary_dict(genotype=mousedata["G"], mouse=mouse)
+
+        self.build_summary_dict(genotype=mousedata["G"], eyfp=mousedata['EYFP'], mouse=mouse)
 
         if not check:
             self.plot_setup()
@@ -286,19 +295,19 @@ class MiniAnalysis:
         self.ypqspan = 2000.0
         ntr = 0
         # for all of the protocols that are included for this cell (identified by number and maybe letters)
-        print('mousedata: ', mousedata)
-        print("mousedata prots: ", mousedata["prots"])
+        print('    mousedata: ', mousedata)
+        print("    mousedata prots: ", mousedata["prots"])
         if len(mousedata["prots"]) == 0:
-            print(" No protocols, moving on")
+            print("  No protocols, moving on")
             return None
-        print('prots: ', mousedata['prots'])
+        print('    prots: ', mousedata['prots'])
         for nprot, dprot in enumerate(mousedata["prots"]):
             if nprot > maxprot:
                 return
             self.nprot = nprot
             self.dprot = dprot
             exclude_traces = []
-            # print('exclusion list: ', mousedata["exclist"])
+            print('    exclusion list: ', mousedata["exclist"])
             if dprot in mousedata["exclist"].keys():
                 # print (mousedata['exclist'], dprot, nprot)
                 exclude_traces = mousedata["exclist"][dprot]
@@ -310,26 +319,31 @@ class MiniAnalysis:
             fx = fn.name
             ext = fn.suffix
             if not check:
-                print("Protocol file: ", fn)
-                print("   sign: ", sign)
-            fn = Path(fn, f"{self.protocol_name:s}_{dprot:03d}")
+                print("   Protocol file: ", fn)
+                print("      sign: ", sign)
+            fn = Path(fn, f"{mousedata['protocol_name']:s}_{dprot:03d}")
+            print("    fn: ", fn)
             split = fn.parts[-4:-1]
-            dataname = ""
-            for i in range(len(split)):
-                dataname = Path(dataname, split[i])
-            print('dataname: ', dataname)
+            # dataname = ""
+            # for i in range(len(split)):
+            #     dataname = Path(dataname, split[i])
+            # print('dataname: ', fn)
             acqr.setProtocol(fn)
-
+            # print(check)
             if not check:
-                print("  Protocol dataname: ", dataname)
+                print("  Protocol dataname: ", fn)
                 print("  exclude traces: ", exclude_traces)
             else:
                 result = acqr.getData(check=True)
                 if result is False:
                     CP('r', f"******* Get data failed to find a file : {str(fn):s}")
-                    CP('r', f"        dataname: {dataname:s}")
+                    CP('r', f"        dataname: {fn:s}")
                 continue
             acqr.getData()
+            # print(len(acqr.data_array))
+            # if isinstance(acqr.data_array, list):
+            #     acqr.data_array = np.ndarray(acqr.data_array)
+            # print(acqr.data_array.shape)
             oktraces = [x for x in range(acqr.data_array.shape[0]) if x not in exclude_traces]
             data = np.array(acqr.data_array[oktraces])
             # clip data to time window NOW
@@ -346,9 +360,9 @@ class MiniAnalysis:
             time_base = acqr.time_base[min_index:max_index]
             time_base = time_base - self.min_time
             if not datanameposted and not check:
-                self.P.figure_handle.suptitle(f"{mouse:s}  {str(dataname):s} : {self.cell_summary['genotype']:s}",
-                    fontsize=9,
-                    weight="bold",
+                self.P.figure_handle.suptitle(f"{mouse:s}\n{str(mousedata):s}\n{self.cell_summary['genotype']:s}",
+                    fontsize=8,
+                    weight="normal",
                 )
                 datanameposted = True
             # data = data * 1e12  # convert to pA
@@ -379,7 +393,7 @@ class MiniAnalysis:
             mpl.close()
         self.plot_individual_events(
             fit_err_limit=50.0,
-            title=f"{str(dataname):s} {self.cell_summary['mouse']:s} {self.cell_summary['genotype']:s}",
+            title=f"{str(mousedata):s} {self.cell_summary['mouse']:s} {self.cell_summary['genotype']:s}",
             pdf=pdf,
         )
 

ephys/mini_analyses/minis_methods.py
@@ -420,7 +420,6 @@ class RSDeconvolve(MiniAnalyses):
     ) -> None:
         self.starttime = timeit.default_timer()        
         self.prepare_data(data) # windowing, filtering and timebase
-        starttime = timeit.default_timer()
                 # if data_nostim is None:
         #     data_nostim = [range(self.Crit.shape[0])]  # whole trace, otherwise remove stimuli
         # else:  # clip to max of crit array, and be sure index array is integer, not float
@@ -469,7 +468,6 @@ class RSDeconvolve(MiniAnalyses):
         dtti = 1. - dtt
         for i in range(1, len(d)):
             d[i] = dtti * d[i-1] + dtt * data[i-1]
-    
         if (hasattr(data, 'implements') and data.implements('MetaArray')):
             info = data.infoCopy()
             #if 'values' in info[0]:
@@ -478,7 +476,6 @@ class RSDeconvolve(MiniAnalyses):
             return MetaArray(d, info=info)
         else:
             return d
-
 
     def identify_events(self,
              data_nostim: Union[list, np.ndarray, None] = None,
@@ -538,30 +535,23 @@ class ZCFinder(MiniAnalyses):
         self.taus = None
         self.template_max = None
         self.idelay = 0
-        self.threshold=2.5
+        self.threshold=2.5  # x SD
         self.method = "zc"
         super().__init__()
 
-    def find_events(
+    def deconvolve(
         self,
         data: np.ndarray,
         data_nostim: Union[list, np.ndarray, None] = None,
+        itrace: int = 0,
         minPeak: float = 0.0,
         minSum: float = 0.0,
         minLength: int = 3,
         verbose: bool = False,
-    ) -> None:
+        ) -> None:
         
         self.prepare_data(data) # windowing, filtering and timebase
         starttime = timeit.default_timer()
-        self.sdthr = self.threshold*np.std(self.data)
-        self.Crit = np.zeros_like(self.data)
-        print('zc sdthr: ', self.sdthr)
-        # if data_nostim is None:
-        #     data_nostim = [range(self.Crit.shape[0])]  # whole trace, otherwise remove stimuli
-        # else:  # clip to max of crit array, and be sure index array is integer, not float
-        #     data_nostim = [int(x) for x in data_nostim if x < self.Crit.shape[0]]
-        # data = FN.lowPass(data,cutoff=3000.,dt = 1/20000.)
 
         events = FN.zeroCrossingEvents(
             self.data,
@@ -571,8 +561,26 @@ class ZCFinder(MiniAnalyses):
             noiseThreshold=self.threshold,
             sign=self.sign,
         )
-        self.onsets = np.array([x[0] for x in events]).astype(int)
+        self.Criterion[itrace] = np.zeros_like(self.data)
+        self.events = events
 
+    def identify_events(self, 
+            data_nostim: Union[list, np.ndarray, None] = None,
+            outlier_scale: float = 3.0,
+            verbose: bool = False,
+        ):
+        starttime = timeit.default_timer()
+        self.sdthr = self.threshold*np.std(self.data)
+        if data_nostim is not None:
+            # clip to max of crit array, and be sure index array is integer, not float
+            for i in range(criterion.shape[0]):
+                criterion[i,:] = criterion[i, [int(x) for x in data_nostim if x < criterion.shape[1]]]
+                
+        print(f"ZC thr:  {self.threshold:.3f}  SDxthr: {self.sdthr:.3e}")
+
+        print(self.events[0])
+        self.onsets = np.array([x['index'] for x in self.events if self.sign*x['peak'] > self.sdthr]).astype(int)
+        print('zc onsets: ', self.onsets)
         # self.summarize(self.data)
         endtime = timeit.default_timer() - starttime
         if verbose:

ephys/mini_analyses/minis_methods_common.py
@@ -17,6 +17,7 @@ per acq4 standards...
 import numpy as np
 import scipy.signal
 from dataclasses import dataclass, field
+import traceback
 from typing import Union, List
 import timeit
 from scipy.optimize import curve_fit
@@ -124,7 +125,8 @@ class MiniAnalyses:
         global_SD:Union[float, None] = None,
         analysis_window:[Union[float, None], Union[float, None]] = [None, None],
         lpf:Union[float, None] = None,
-        hpf:Union[float, None] = None
+        hpf:Union[float, None] = None,
+        notch:Union[float, None] = None,
     ) -> None:
         """
         Just store the parameters - will compute when needed
@@ -152,6 +154,8 @@ class MiniAnalyses:
         self.analysis_window = analysis_window
         self.lpf = lpf
         self.hpf = hpf
+        self.notch = notch
+        self.reset_filtering()
 
     def set_sign(self, sign: int = 1):
         self.sign = sign
@@ -164,6 +168,18 @@ class MiniAnalyses:
             self.risepower = risepower
         else:
             raise ValueError("Risepower must be 0 < n <= 8")
+
+    # def set_notch(self, notches):
+    #     if isinstance(nothce, float):
+    #         notches = [notches]
+    #     elif isinstance(notches, None):
+    #         self.notch = None
+    #         self.Notch_applied = False
+    #         return
+    #     elif isinstance(notches, list):
+    #         self.notch = notches
+    #     else:
+    #         raise ValueError("set_notch: Notch must be list, float or None")
 
     def _make_template(self):
         """
@@ -197,6 +213,7 @@ class MiniAnalyses:
     def reset_filtering(self):
         self.filtering.LPF_applied = False
         self.filtering.HPF_applied = False
+        self.filtering.Notch_applied = False
         
     def LPFData(
         self, data: np.ndarray, lpf: Union[float, None] = None, NPole: int = 8
@@ -252,6 +269,27 @@ class MiniAnalyses:
         self.filtering.HPF_applied = True
 
         return data
+
+    # def NotchData(self, data:np.ndarray, notch: Union[list, None] = None) -> np.ndarray:
+    #     assert (not self.filtering.notch_applied)  # block repeated application of filtering
+    #     if notch is None or len(notch) == 0 :
+    #         return data
+    #     if len(data.shape) == 1:
+    #         ndata = data.shape[0]
+    #     else:
+    #         ndata = data.shape[1]
+    #
+    #     data[i] = dfilt.NotchFilter(
+    #                 data[i]-data[0],
+    #                 notch,
+    #                 Q=20.0,
+    #                 samplefreq=1.0 / self.dt_seconds,
+    #             )
+    #     self.filtering.notch = notch
+    #     self.filtering.Notch_applied = True
+    #
+    #     return data
+
     
     def prepare_data(self, data):
         """
@@ -284,6 +322,8 @@ class MiniAnalyses:
             data = self.LPFData(data, lpf=self.lpf)
         if isinstance(self.hpf, float):
             data = self.HPFData(data, hpf=self.hpf)
+        # if isinstance(self.notch, list):
+        #     data = self.HPFData(data, notch=self.notch)
         self.data = data
         self.timebase = self.timebase[jmin:jmax]
 
@@ -328,7 +368,6 @@ class MiniAnalyses:
         avgwin = (
             5  # int(1.0/self.dt_seconds)  # 5 point moving average window for peak detection
         )
-
         mwin = int((0.50) / self.dt_seconds)
         if self.sign > 0:
             nparg = np.greater
@@ -350,11 +389,14 @@ class MiniAnalyses:
             for j, onset in enumerate(self.onsets[itrial]):  # for all of the events in this trace
                 if self.sign > 0 and self.eventstartthr is not None:
                     if dataset[onset] < self.eventstartthr:
+                        # print('pos sign: data onset < eventstartthr')
                         continue
                 if self.sign < 0 and self.eventstartthr is not None:
                     if dataset[onset] > -self.eventstartthr:
+                        # print('neg sign: data onset > eventstartthr')
                         continue
                 event_data = dataset[onset : (onset + mwin)]  # get this event
+                # print('onset, mwin: ', onset, mwin)
                 svwinlen = event_data.shape[0]
                 if svwinlen > 11:
                     svn = 11
@@ -373,7 +415,6 @@ class MiniAnalyses:
                         order=order,
                     )[0]
                 else:  # skip filtering
-                    print('mmethods_common summarize order: ', order)
                     p = scipy.signal.argrelextrema(
                         event_data,
                         nparg,
@@ -430,6 +471,7 @@ class MiniAnalyses:
         self.average_events(
             data,
         )
+        # print(self.Summary.average.avgevent)
         if self.Summary.average.averaged:
             self.fit_average_event(
                 tb=self.Summary.average.avgeventtb,
@@ -521,24 +563,34 @@ class MiniAnalyses:
             # cprint('c', f"Trace: {itrace: d}, # onsets: {len(onsets):d}")
             for j, event_onset in enumerate(onsets):
                 ix = event_onset + pkt  # self.idelay
-                # print('itrace, ix, npre, npost: ', itrace, ix, npre, npost)
+                # print('itrace, ix, npre, npost: ', itrace, ix, npre, npost, data[itrace].shape[0])
                 if (ix + npost) < data[itrace].shape[0] and (ix - npre) >= 0:
                     allevents[k, :] = data[itrace, (ix - npre) : (ix + npost)]
                 else:
+                    print("oops, nan events")
                     allevents[k, :] = np.nan*allevents[k,:]
                 event_trace[k] = [itrace, j]
                 k = k + 1
         # tr_incl = [u[0] for u in event_trace]
         # print(set(tr_incl), len(set(tr_incl)), len(event_trace))
         # exit()
+        # print('k: ', k)
         if k > 0:
             self.Summary.average.averaged = True
             self.Summary.average.avgnpts = avgnpts
             self.Summary.average.Nevents = k
             self.Summary.allevents = allevents
-            avgevent = allevents.mean(axis=0)
-            self.Summary.average.avgevent = avgevent - np.mean(avgevent[:3])
             self.Summary.average.avgeventtb = avgeventtb
+            avgevent = np.nanmean(allevents, axis=0)
+            # print(allevents)
+            # import matplotlib.pyplot as mpl
+            # f, ax = mpl.subplots(1,1)
+            # ax.plot(allevents.T, 'k', alpha=0.3)
+            # ax.plot(avgevent, 'r', linewidth=3)
+            # mpl.show()
+            # print(avgevent)
+            # exit(1)
+            self.Summary.average.avgevent = avgevent - np.mean(avgevent[:3])
             self.Summary.event_trace_list = event_trace
             return
         else:
@@ -632,13 +684,26 @@ class MiniAnalyses:
         p is in order: [amplitude, tau, delay]
         """
         assert mode in [-1, 0, 1]
+        # if np.isnan(p[0]):
+        #     try:
+        #         x = 1./p[0]
+        #     except Exception as e:
+        #         track = traceback.format_exc()
+        #         print(track)
+        #         exit(0)
+        # # assert not np.isnan(p[0])
         ix = np.argmin(np.fabs(x - p[2]))
         tm = np.zeros_like(x)
         expf = (x[ix:] - p[2]) / p[1]
         pclip = 1.0e3
         nclip = 0.0
-        expf[expf > pclip] = pclip
-        expf[expf < -nclip] = -nclip
+        try:
+            expf[expf > pclip] = pclip
+            expf[expf < -nclip] = -nclip
+        except:
+            print(pclip, nclip)
+            print(expf)
+            exit(1)
         tm[ix:] = p[0] * (1.0 - np.exp(-expf)) ** risepower
         if mode == 0:
             return tm - y
@@ -703,6 +768,7 @@ class MiniAnalyses:
         # init_vals = [self.sign*10.,  1.0,  4., 0.]
         # init_vals_exp = [20.,  5.0]
         # bounds_exp  = [(0., 0.5), (10000., 50.)]
+        cprint('m', 'Fitting average event')
 
         res, rdelay = self.event_fitter(
             tb,
@@ -713,7 +779,9 @@ class MiniAnalyses:
             label=label,
         )
         # print('rdelay: ', rdelay)
+
         if res is None:
+            cprint('r', 'average fit result is None')
             self.fitted = False
             return
         self.fitresult = res.x
@@ -885,6 +953,12 @@ class MiniAnalyses:
         variation that is present in terms of rise-fall tau tradeoffs.
         
         """
+        if len(event) == 0:  # just check against emtpy event
+            cprint('m', 'Event length is 0? ')
+            # assert 1 == 0
+            return None, None
+        else:
+            cprint('c', f'event length is {len(event):d}')
         debug = False
         if debug:
             import matplotlib.pyplot as mpl
@@ -892,9 +966,12 @@ class MiniAnalyses:
             init_delay = 1
         else:
             init_delay = int(initdelay / self.dt_seconds)
-
         ev_bl = np.mean(event[: init_delay])  # just first point...
+        # print("sign, event, bl: ", self.sign, len(event), init_delay)
+        # print('event: ', event)
+        # print('bl: ', ev_bl)
         evfit = self.sign * (event - ev_bl)
+        # print('evfit0: ', evfit)
         maxev = np.max(evfit)
         if maxev == 0:
             maxev = 1
@@ -902,6 +979,8 @@ class MiniAnalyses:
         #     peak_pos = int(0.001/self.dt_seconds) # move to 1 msec later
         evfit = evfit / maxev  # scale to max of 1
         peak_pos = np.argmax(evfit) + 1
+        if peak_pos == 1:
+            peak_pos = 5
         amp_bounds = [0.0, 1.0]
         # set reasonable, but wide bounds, and make sure init values are within bounds
         # (and off center, but not at extremes)
@@ -916,6 +995,11 @@ class MiniAnalyses:
         init_vals_rise = [0.9, self.dt_seconds * peak_pos, fdelay]
         
         cprint("r", "event_fitter: rise")
+        # print('    initvals: ', init_vals_rise)
+        # print('    bounds: ', bounds_rise)
+        # print('    peak_pos: ', peak_pos)
+        # print('   evfit: ', evfit[:peak_pos])
+        # print('maxev: ', maxev)
         try:
             res_rise = scipy.optimize.minimize(
                 self.risefit,
@@ -930,12 +1014,13 @@ class MiniAnalyses:
                 ),  # risepower, mode
             )
         except:
-            # mpl.plot(self.timebase[:peak_pos], evfit[:peak_pos], 'k-')
-  #           mpl.show()
-  #           print('risefit: ', self.risefit)
-  #           print('init_vals_rise: ', init_vals_rise)
-  #           print('bounds rise: ', bounds_rise)
-  #           print('peak_pos: ', peak_pos)
+            import matplotlib.pyplot as mpl
+            mpl.plot(self.timebase[:peak_pos], evfit[:peak_pos], 'k-')
+            mpl.show()
+            print('risefit: ', self.risefit)
+            print('init_vals_rise: ', init_vals_rise)
+            print('bounds rise: ', bounds_rise)
+            print('peak_pos: ', peak_pos)
             return None, None
             # raise ValueError()
             
@@ -958,7 +1043,7 @@ class MiniAnalyses:
             ax[0].plot(rise_tb, rise_yfit, "r-")
             ax[1].plot(rise_tb, rise_yfit, "r-")
             # mpl.show()
-
+        # cprint('c', f"Res_rise: {str(res_rise):s}")
         self.res_rise = res_rise
         # fit decay exponential next:
         bounds_decay = [