1360adfbb800b1fed7f982c638906aa0d41a017c
Paul Manis <pmanis@med.unc.edu>
2021-06-28 13:34:37 -0400

    adjustments for py3.9 and to make work with AllenSDK

--- make_local.sh
+++ make_local.sh
@@ -1,9 +1,10 @@
 ENVNAME="ephys_venv"
-python3.7 -m venv $ENVNAME
+python3.8 -m venv $ENVNAME
 source $ENVNAME/bin/activate
 pip3 install --upgrade pip  # be sure pip is up to date in the new env.
 pip3 install wheel  # seems to be missing (note singular)
 pip3 install cython
+
 # # if requirements.txt is not present, create:
 # # pip install pipreqs
 # # pipreqs
@@ -12,5 +13,5 @@ pip3 install cython
 #
 pip3 install -r requirements_local.txt
 source $ENVNAME/bin/activate
-python3.7 --version
+python3.8 --version
 python setup.py develop
