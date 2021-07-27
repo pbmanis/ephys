037bf9bcc1d24af806cb6c71ed6c5c0a4ea53136
Paul Manis <pmanis@med.unc.edu>
2021-07-27 12:20:24 -0400

    almost there

--- ephys/tools/fix_objscale.py
+++ ephys/tools/fix_objscale.py
@@ -11,17 +11,17 @@ import sys
 from dataclasses import dataclass
 from typing import Union
 import numpy as np
-import argparse
 from pathlib import Path
 from pyqtgraph import configfile
 import pprint
-from collections import OrderedDict
 import datetime
 import pyqtgraph as pg
 import pyqtgraph.dockarea as PGD
 from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
 from pylibrary.tools import fileselector as FS
+from pylibrary.tools.utility import seqparse as SQP
 from pyqtgraph.parametertree import Parameter, ParameterTree
+import toml
 
 import pylibrary.tools.cprint as CP
 
@@ -52,7 +52,6 @@ class Changer:
     images: Union[str, None] = None
 
 
-
 class FixObjective(pg.QtGui.QWidget):
     def __init__(self, app=None):
         super(FixObjective, self).__init__()
@@ -70,54 +69,63 @@ class FixObjective(pg.QtGui.QWidget):
         else:
             if self.filelistpath.is_file():
                 file_dict = toml.load(self.filelistpath)
-                current_filename = file_dict['MostRecent']
+                current_filename = file_dict["MostRecent"]
             else:
-                print('No Previous Files Found')
+                print("No Previous Files Found")
                 return
         self.objdata.filename = current_filename
         self.read_index(objective=self.objdata, write=False)
         self.show_index()
-        
+
     def rewrite_index(self, index: dict, index_file: Union[str, Path]):
         configfile.writeConfigFile(index, index_file)
 
-
-    def read_indexes(self,changelist: list = [], write: bool = False):
+    def read_indexes(self, changeList: list = [], write: bool = False):
         """
         To change a list of images/videos
         """
         print("write: ", write)
         for objective in changeList:
-            index = read_index(index_path, objective, write=write)
+            self.read_index(objective, write=write)
 
     def read_index(self, objective: object = None, write: bool = False):
-        print("\nfix_objscale: We will be using the following reference scale: ", refscale)
+        print(
+            "\nfix_objscale: We will be using the following reference scale: ", refscale
+        )
         print("   This scale may be specific to your camera!!!!!")
         print("read_index write flag is: ", write)
-        nl = "\n"
-        print(f"Objective: ")
+        print("Objective: ")
         print(f"    Change Type: {objective.change_type:s}")
         print(f"    File: {objective.filename:s}")
-        print(f"    From: {objective.from_objective:3s}X to {objective.to_objective:3s}X")
+        print(
+            f"    From: {objective.from_objective:3s}X to {objective.to_objective:3s}X"
+        )
         print(f"    For images: {str(objective.images):s}")
         print(f"    For videos: {str(objective.videos):s}")
-        print("="*40)
+        print("=" * 40)
         self.index_file = Path(objective.filename, ".index")
         self.index = configfile.readConfigFile(self.index_file)
-        return self.index
 
     def show_index(self):
         print("self.index: ")
         # print(self.index)
         self.textbox.setCurrentFont(QtGui.QFont("Courier New"))
-        text = [] # ["<font color='red' size='3' font-family='monospace'>Hello PyQt5!\nHello"]
+        text = (
+            []
+        )  # ["<font color='red' size='3' font-family='monospace'>Hello PyQt5!\nHello"]
         # ["<font-family:'Courier New' color='blue' size='10'>"]
         for k in self.index.keys():
             print("index is: ", k)
             if k.startswith("image"):
-                text.append(f" {k:16s}: {self.index[k]['objective']:24s}, {str(self.index[k]['binning']):8s} {str(self.index[k]['transform']['scale']):s}<br>")
+                text.append(
+                    f" {k:16s}: {self.index[k]['objective']:24s}, {str(self.index[k]['binning']):8s}"
+                    + f" {str(self.index[k]['transform']['scale']):s}<br>"
+                )
             if k.startswith("video"):
-                text.append(f" {k:16s}: {self.index[k]['objective']:24s}, {str(self.index[k]['binning']):8s} {str(self.index[k]['transform']['scale']):s}<br>")
+                text.append(
+                    f" {k:16s}: {self.index[k]['objective']:24s}, {str(self.index[k]['binning']):8s}"
+                    + f" {str(self.index[k]['transform']['scale']):s}<br>"
+                )
 
         self.textbox.setHtml("\n".join([t for t in text]))
 
@@ -125,93 +133,117 @@ class FixObjective(pg.QtGui.QWidget):
         self.objdata.to_objective = data
 
     def update_from_objective(self):
-        fn = self.get_imagefilename(self.objdata)  # get for the current
-        # print("fn: ", fn)
+        fns = self.get_imagefilenames(self.objdata)  # get for the current
+        print("fns: ", fns)
         # print('self.index: ', self.index)
-        objname = self.index[fn]['objective']
-        if objname not in list(objectiveDict.keys()):
-            print(f"Objective {objname:s} not found in list of known objectives.")
-            return
-        print("objname: ", objname)
-        self.ptreedata.param('Original Objective').setValue(objname)
+        for fn in fns:
+            objname = self.index[fn]["objective"]
+            if objname not in list(objectiveDict.keys()):
+                print(f"Objective {objname:s} not found in list of known objectives.")
+                continue
+            print("objname: ", objname)
+            self.ptreedata.param("Original Objective").setValue(objname)
 
-        
     def get_original_objective(self):
         pass
+
     def view_proposed_changes(self):
         print(self.objdata)
         self.change_scale(self.objdata, write=False)
-    
-    def get_imagefilename(self, objective: object) -> str:
-        if objective.images is not None:
-            imagefile = f"image_{int(objective.images):03d}.tif"
-        elif objective.videos is not None:
-            imagefile = f"video_{int(objective.videos):03d}.ma"
+
+    def get_imagefilenames(self, objective: object) -> str:
+        imagefiles = []
+        print('obj img: ', objective.images)
+        if objective.images[0] is not None:
+            images = SQP(objective.images)[0]
+            for img in images:
+                print("img: ", img)
+                imagefiles.append(f"image_{int(img):03d}.tif")
+        elif objective.images[1] is not None:
+            videos = SQP(objective.images[1])[0]
+            for vid in videos:
+                imagefiles.append(f"img_{int(vid):03d}.tif")
         else:
-            imagefile = None
-        return imagefile
-        
-    def change_scale(self, objective:object=None, write:bool=False):
-        imagefile = self.get_imagefilename(objective)
-        if imagefile is None:
+            pass
+        return imagefiles
+
+    def change_scale(self, objective: object = None, write: bool = False):
+        imagefiles = self.get_imagefilenames(objective)
+        if len(imagefiles) == 0:
             return
 
         print("\n----------------------------")
-        print(imagefile)
+        print(imagefiles)
         print(self.index.keys())
-        if imagefile not in self.index.keys():
-            CP.cprint("m", f"File {imagefile:s} not found in {str(list(self.index.keys())):s}")
-            return
-        k = imagefile
-        print("Index: ", k)
-        print("Old objective: ", self.index[k]["objective"])  # pp.pprint(index[k] )
-        old_objective = self.index[k]["objective"]
-        pp.pprint("   Old transform: ")
-        pp.pprint(self.index[k]["transform"])
-        pp.pprint("   Old device transform,: ")
-        pp.pprint(self.index[k]["deviceTransform"])
-        binning = self.index[k]["binning"]
-        new_objective = objective.to_objective # string name
-        f_new_objective = float(objectiveDict[new_objective])  # new magnification
-        self.index[k]["transform"]["scale"] = (
-            binning[0] * refscale[0] / f_new_objective,
-            binning[1] * refscale[1] / f_new_objective,
-            1.0,
-        )
-        self.index[k]["deviceTransform"]["scale"] = (
-            binning[0] * refscale[0] / f_new_objective,
-            binning[1] * refscale[1] / f_new_objective,
-            1.0,
-        )
-        d = datetime.datetime.now()
-        dstr = d.strftime("%Y-%m-%d %H:%M:%S")
-        self.index[k]["objective"] = objectiveDict[objective.to_objective]
-        self.index[k][
-            "note"
-        ] = f"Objective scale corrected from {str(objectiveDict[old_objective]):s} to {str(objectiveDict[new_objective]):s} on {dstr:s} by PBM"
-        print("New objective: ", self.index[k]["objective"])  # pp.pprint(index[k] )
-        print("   New transform: ")
-        pp.pprint(self.index[k]["transform"])
-        print("   New device transform: ")
-        pp.pprint(self.index[k]["deviceTransform"])
-        print("   Added Note: ", self.index[k]["note"])
-        print("----------------------------")
+        for imagefile in imagefiles:
+            if imagefile not in self.index.keys():
+                CP.cprint(
+                    "m",
+                    f"File {imagefile:s} not found in {str(list(self.index.keys())):s}",
+                )
+                continue
+            print("Index imagefile: ", imagefile)
+            print(
+                "Old objective: ", self.index[imagefile]["objective"]
+            )  # pp.pprint(index[imagefile] )
+            old_objective = self.index[imagefile]["objective"]
+            pp.pprint("   Old transform: ")
+            pp.pprint(self.index[imagefile]["transform"])
+            pp.pprint("   Old device transform,: ")
+            pp.pprint(self.index[imagefile]["deviceTransform"])
+            binning = self.index[imagefile]["binning"]
+            new_objective = objective.to_objective  # string name
+            magnification_new_objective = float(
+                objectiveDict[new_objective]
+            )  # new magnification
+            self.index[imagefile]["transform"]["scale"] = (
+                binning[0] * refscale[0] / magnification_new_objective,
+                binning[1] * refscale[1] / magnification_new_objective,
+                1.0,
+            )
+            self.index[imagefile]["deviceTransform"]["scale"] = (
+                binning[0] * refscale[0] / magnification_new_objective,
+                binning[1] * refscale[1] / magnification_new_objective,
+                1.0,
+            )
+            d = datetime.datetime.now()
+            dstr = d.strftime("%Y-%m-%d %H:%M:%S")
+            self.index[imagefile]["objective"] = objective.to_objective
+            self.index[imagefile][
+                "note"
+            ] = f"Objective scale corrected from {str(objectiveDict[old_objective]):s}"
+            self.index[imagefile][
+                "note"
+            ] += f" to {str(objectiveDict[new_objective]):s} on {dstr:s} by PBM"
+            print(
+                "New objective: ", self.index[imagefile]["objective"]
+            )  # pp.pprint(index[imagfile] )
+            print("   New transform: ")
+            pp.pprint(self.index[imagefile]["transform"])
+            print("   New device transform: ")
+            pp.pprint(self.index[imagefile]["deviceTransform"])
+            print("   Added Note: ", self.index[imagefile]["note"])
+            print("----------------------------")
 
-        print("read_index write: ", write)
-        if write:
-            rewrite_index(self.index, index_file)
-            CP.cprint("g", ".index file has been updated")
+            print("read_index write: ", write)
+            index_filename = Path(objective.filename, ".index")
+            if write:
+                self.rewrite_index(self.index, index_filename)
+                CP.cprint("g", ".index file has been updated")
 
-        else:
-            print("Dry Run: .index file was NOT modified")
+            else:
+                print("Dry Run: .index file was NOT modified")
+                print(f" file to modify is: {str(index_filename):s}")
+        # then update index file display
+        self.show_index()
 
     def build_ptree(self):
+        self.ptreewid = 300
         self.params = [
             # {"name": "Pick Cell", "type": "list", "values": cellvalues, "value": cellvalues[0]},
             {"name": "Set Directory/Protocol", "type": "action"},
             {"name": "Reload Last Protocol", "type": "action"},
             {"name": "View .index", "type": "action"},
-            
             {
                 "name": "Original Objective",
                 "type": "list",
@@ -226,35 +258,19 @@ class FixObjective(pg.QtGui.QWidget):
                 "value": self.objdata.to_objective,
                 "renamable": False,
             },
-            {
-                "name": "Images",
-                "type": "str",
-                "value": "",
-            },
-            {
-                "name": "Videos",
-                "type": "str",
-                "value": "",
-            },
-            {
-                "name": "View proposed changes",
-                "type": "action"
-            },
-            {
-                "name": "Quit",
-                "type": "action",
-            },
-            
+            {"name": "Images", "type": "str", "value": ""},
+            {"name": "Videos", "type": "str", "value": ""},
+            {"name": "View proposed changes", "type": "action"},
+            {"name": "Quit", "type": "action"},
         ]
         self.ptree = ParameterTree()
         self.ptreedata = Parameter.create(
             name="Commands", type="group", children=self.params
         )
-        
-        self.ptree.setParameters(self.ptreedata)
-        self.ptree.setMaximumWidth(300)
-        self.ptree.setMinimumWidth(250)
 
+        self.ptree.setParameters(self.ptreedata)
+        self.ptree.setMaximumWidth(self.ptreewid)
+        self.ptree.setMinimumWidth(self.ptreewid-100)
 
     def command_dispatcher(self, param, changes):
         """
@@ -281,12 +297,12 @@ class FixObjective(pg.QtGui.QWidget):
             elif path[0] == "View proposed changes":
                 self.view_proposed_changes()
             elif path[0] == "Videos":
-                self.objdata.videos = int(data)
+                self.objdata.videos = data
             elif path[0] == "Images":
-                self.objdata.images = int(data)
+                self.objdata.images = data
                 print(self.objdata.images)
                 self.update_from_objective()
-                
+
             elif path[0] == "Quit":
                 self.quit()
 
@@ -300,29 +316,33 @@ class FixObjective(pg.QtGui.QWidget):
         layout.setSpacing(8)
         self.win.setCentralWidget(self.dockArea)
         self.win.setWindowTitle("Model DataTables/FileSelector")
-        self.win.resize(850, 400)
+        win_wid = 1024
+        win_ht = 512
+        self.win.resize(1024, 512)
         self.win.setWindowTitle("No File")
-        
+
         self.buttons = pg.QtGui.QGridLayout()
         self.build_ptree()
         self.buttons.addWidget(self.ptree)
         self.ptreedata.sigTreeStateChanged.connect(self.command_dispatcher)
 
         # Initial Dock Arrangment
-        self.Dock_Params = PGD.Dock("Params", size=(350, 400))
+        par_wid = self.ptreewid
+        
+        self.Dock_Params = PGD.Dock("Params", size=(par_wid, win_ht))
         self.Dock_Params.addWidget(self.ptree)
-        self.Dock_Report = PGD.Dock("Reporting", size=(500, 400))
+        self.Dock_Report = PGD.Dock("Reporting", size=(win_wid-par_wid, win_ht))
 
         self.textbox = QtWidgets.QTextEdit()
         self.textbox.setReadOnly(True)
-        self.textbox.setText("Text Edit box (RO)")
-        self.Dock_Report.addWidget(self.textbox) 
+        self.textbox.setText("(.index file)")
+        self.Dock_Report.addWidget(self.textbox)
 
         self.dockArea.addDock(self.Dock_Params, "left")
         self.dockArea.addDock(self.Dock_Report, "right", self.Dock_Params)
-        
+
         self.win.show()
-    
+
     def quit(self):
         exit(0)
 
@@ -337,8 +357,6 @@ def main():
 
     if (sys.flags.interactive != 1) or not hasattr(pg.QtCore, "PYQT_VERSION"):
         pg.QtGui.QApplication.instance().exec_()
-
-
 
 
 # old command line version
