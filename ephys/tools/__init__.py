a767ea002eb803a8f52d6c643dabb9c77aa5fc07
Paul Manis <pmanis@med.unc.edu>
2021-10-18 15:55:13 -0400

    keep up with the changes

--- ephys/tools/__init__.py
+++ ephys/tools/__init__.py
@@ -3,7 +3,8 @@ from . import bridge
 from . import cursor_plot
 from . import fix_objscale
 from . import plot_maps
-from . import show_data
+from . import miniviewer
+from . import minicalcs
 from . import digital_filters
 
 
