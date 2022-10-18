4460f203842fbcb2d4775c4ac0beef0925eb801a
Paul Manis <pmanis@med.unc.edu>
2022-10-18 14:22:54 -0400

    clean up, reorganize some, and handle NWB input format

--- ephys/ephysanalysis/MakeClamps.py
+++ ephys/ephysanalysis/MakeClamps.py
@@ -4,56 +4,59 @@ from simple data (time, traces, command)
 This is used as a base class for vcnmodel readmodel to read a pickled data file
 
 """
-from pathlib import Path
-import numpy as np
-import pickle
-import matplotlib
+
 from dataclasses import dataclass
-import dataclasses
-import datetime
-import matplotlib.pyplot as mpl
-import pylibrary.plotting.plothelpers as PH
-from pylibrary.tools.params import Params
-import MetaArray as EM 
+from typing import Union
+
+import numpy as np
+import MetaArray as EM
 
 
 class MakeClamps:
     """
     Make acq4 clamp structures from data passed from other formats
     """
-    
-    def __init__(self):
-        self.holding = 0.  # set a default value for models
-        self.WCComp = 0.
-        self.CCComp = 0.
-        self.NWB = False
 
-    
-    def set_clamps(self, time, data, cmddata=None, dmode='CC', tstart_tdur=[0.01, 0.100], protocol=None):
+    def __init__(self):
+        self.holding = 0.0  # set a default value for models
+        self.WCComp = 0.0
+        self.CCComp = 0.0
+        self.NWB = False
+        self.nwbfile = None
+
+    def set_clamps(
+        self,
+        time,
+        data,
+        cmddata=None,
+        dmode="CC",
+        tstart_tdur=[0.01, 0.100],
+        protocol=None,
+    ):
         """
         Copy parameters into the Clamps structure.
-        
+
         Parameters
         ----------
         time: np.array (no default)
             An array holding the time data. Should be same length as the data
-        
+
         data: np.array or ndarray (no default)
-            An array of the data 
-        
+            An array of the data
+
         cmddata: np.array (default None)
             Command waveform data if available
-        
+
         dmode : str (default: 'CC')
             data mode: CC for current clamp, vc for voltage clamp
-        
+
         tatsrt_tdur: 2-element list or np.array (default: [0.01, 0.10]; values in seconds)
             The start and duration for current/voltage pulses and steps.
-        
+
         """
         self.data = data
         self.time = time
-        self.rate = np.diff(self.time)*1e6
+        self.rate_sec = np.diff(self.time)
         self.cmddata = cmddata
         self.tstart_tdur = tstart_tdur
         self.tstart = tstart_tdur[0]
@@ -61,11 +64,12 @@ class MakeClamps:
         self.dmode = dmode
         self.protocol = protocol
 
-    def setNWB(self, nwbmode:bool = False):
+    def setNWB(self, nwbmode: bool = False, nwbfile: Union[object, None] = None):
         self.NWB = nwbmode
+        self.nwbfile = nwbfile
 
     def getData(self):
-        return(self.getClampData())
+        return self.getClampData()
 
     def getClampData(self):
         """
@@ -100,7 +104,7 @@ class MakeClamps:
         self.tdur
         self.tend
         self.spikecount = np.zeros(len...) if in vcmode.
-        
+
         Info from an example data file:
         [{'name': 'Channel', 'cols': [{'units': 'A', 'name': 'Command'}, {'units': 'V', 'name': 'primary'}, {'units': 'A', 'name': 'secondary'}]},
         {'units': 's', 'values': array([ 0.00000000e+00, 2.50000000e-05, 5.00000000e-05, ..., 6.99925000e-01, 6.99950000e-01, 6.99975000e-01]),
@@ -118,118 +122,187 @@ class MakeClamps:
         40000.0, 'type': 'ai', 'startTime': 1296241556.7347913}}, 'startTime': 1296241556.7347913}]
 
         )
-        
+
         Parameters
         ----------
         None
-            
+
         """
         if self.data is None:
-            raise ValueError('No data has been set')
+            raise ValueError("No data has been set")
         protocol = self.protocol
-
-        self.sample_interval = self.rate[0]*1e-6  # express in seconds
-        self.traces = np.array(self.data)
-        # nchannels = self.data.shape[0]
-        dt = self.sample_interval  # make assumption that rate is constant in a block
-        if self.NWB:
-            time_ax = 0
-            rec_ax = 1
-        else:
-            time_ax = 1
-            rec_ax = 0
-        self.time_base = self.time[:self.traces.shape[time_ax]] # in seconds
-        points = self.data.shape[time_ax]
-        recs = range(self.data.shape[rec_ax])
-
-        if self.dmode == 'CC':  # use first channel
-            mainch = 0
-            cmdch = 1
-        else:  # assumption is swapped - for this data, that means voltage clamp mode.
-            mainch = 1
-            cmdch = 0
-
-        cmds = self.cmddata # self.traces[:,cmdch,:]
-        self.tstart = self.tstart_tdur[0]  # could be pulled from protocol/stimulus information
-        self.tdur = self.tstart_tdur[1]
-        self.tend = self.tstart + self.tdur
-        t0 = int(self.tstart/dt)
-        t1 = int(self.tend/dt)
-        self.cmd_wave = self.cmddata # np.squeeze(self.traces[:, cmdch, :])
-        diffpts = self.traces.shape[time_ax] - self.cmd_wave.shape[time_ax]
-        ntr = self.cmd_wave.shape[rec_ax]
-        if diffpts > 0:
-            self.cmd_wave = np.pad(self.cmd_wave, pad_width=(0, diffpts), 
-                    mode='constant', constant_values=0.)[:, :ntr]  # awkward
-        self.holding = np.zeros((ntr, 1))
-        if cmds.shape[0] > 1:
-            if self.NWB:
-                self.values = np.nanmean(self.cmd_wave[t0:t1, :], axis=time_ax)  # express values in amps
-                self.holding = np.nanmean(self.cmd_wave[:t0, :], axis=time_ax)
-                self.cmd_wave = self.cmd_wave.T
-                self.traces = self.traces.T
-            else:
-                self.values = np.nanmean(self.cmd_wave[:, t0:t1], axis=time_ax)  # express values in amps
-                self.holding = np.nanmean(self.cmd_wave[:, :t0], axis=time_ax)
-        else:
-            self.values = np.zeros_like(self.traces.shape[1:2])
-        self.commandLevels = self.values        
-        # for i in range(self.traces.shape[0]):
-        #     mpl.plot(self.time, self.traces[i])
-        #     mpl.plot(self.time[:self.cmd_wave[i].shape[0]], self.cmd_wave[i])
-        # mpl.show()
-        
-        info = [{'units': 'A', 'values': self.values, 'name': 'Command'},  # dict 0
-                    {'name': 'Time', 'units': 's', 'values': self.time_base},  # dict 1
-                    {'ClampState':  # note that many of these values are just defaults and cannot be relied upon
-                        {'primaryGain': 1.0, 'ClampParams': 
-                            {'OutputZeroEnable': 0, 'PipetteOffset': 0.0,
-                            'Holding': 0., 'PrimarySignalHPF': 0.0, 'BridgeBalResist': 0.0, 
-                            'PrimarySignalLPF': 20000.0, 'RsCompBandwidth': 0.0, 
-                            'WholeCellCompResist': 0.0, 'WholeCellCompEnable': 6004, 'LeakSubResist': 0.0,
-                            'HoldingEnable': 1, 'FastCompTau': 0.0, 'SlowCompCap': 0.0, 
-                            'WholeCellCompCap': 0.,
-                            'LeakSubEnable': 6004, 'NeutralizationCap': 0.,
-                            'BridgeBalEnable': 0, 'RsCompCorrection': 0.0,
-                            'NeutralizationEnable': 1, 'RsCompEnable': 6004,
-                            'OutputZeroAmplitude': 0., 'FastCompCap': 0.,
-                            'SlowCompTau': 0.0}, 'secondarySignal': 
-                            'Command Current', 'secondaryGain': 1.0,
-                            'secondaryScaleFactor': 2e-09,
-                            'primarySignal': 'Membrane Potential', 'extCmdScale': 4e-10,
-                            'mode': self.dmode, 'holding': self.holding, 'primaryUnits': 'V', 
-                            'LPFCutoff': 10000.,
-                            'secondaryUnits': 'A', 'primaryScaleFactor': 0.1,
-                            'membraneCapacitance': 0.0,
-                        }, 
-                    'Protocol': {'recordState': True, 'secondary': None,
-                                'primary': None, 'mode': 'IC',
-                            }, 
-                    'DAQ': {'command': {'numPts': points, 'rate': self.sample_interval,
-                                'type': 'ao', 'startTime': 0.},
-                                'primary': {'numPts': points, 'rate': self.sample_interval,
-                                'type': 'ai', 'startTime': 0.}, 
-                                'secondary': {'numPts': points, 'rate': self.sample_interval,
-                                'type': 'ai', 'startTime': 0.},
-                            },
-                    'startTime': 0.}
-                ]
 
         # filled, automatically with default values
         self.repc = 1
         self.nrepc = 1
         self.model_mode = False
         self.command_scale_factor = 1
-        self.command_units = 'A'
+        self.command_units = "A"
         self.devicesUsed = None
         self.clampDevices = None
-        self.holding = 0.
-        self.amplfierSettings = {'WCCompValid': False, 'WCEnabled': False, 
-                'CompEnabled': False, 'WCSeriesResistance': 0.}
+        self.holding = 0.0
+        self.bridgeBalance = 0.0
+        self.neutralizingCap = 0.0
+        self.primarySignalLPF = 20000.
+        self.amplfierSettings = {
+            "WCCompValid": False,
+            "WCEnabled": False,
+            "CompEnabled": False,
+            "WCSeriesResistance": 0.0,
+        }
         self.clampState = None
-        self.RSeriesUncomp = 0.
-            
-        self.tend = self.tstart + self.tdur
+        self.RSeriesUncomp = 0.0
+        if self.NWB:  # fill some parameters from the nwbfile instead of just using defaults
+            step_times = self.nwbfile.acquisition["Ics1"].control
+            self.tstart = step_times[0]*self.rate_sec[0]
+            self.tend = step_times[1]*self.rate_sec[0]
+            self.tdur = self.tend - self.tstart
+            self.holding = self.nwbfile.acquisition["Vcs1"].bias_current
+            self.bridgeBalance = self.nwbfile.acquisition["Vcs1"].bridge_balance
+            self.neutralizingCap = self.nwbfile.acquisition[
+                "Vcs1"
+            ].capacitance_compensation
+            self.primarySignalLPF = self.nwbfile.acquisition["Vcs1"].electrode.filtering
+
+        else:
+            self.tstart = self.tstart_tdur[
+                0
+            ]  # should be pulled from protocol/stimulus information
+            self.tdur = self.tstart_tdur[1]
+            self.tend = self.tstart + self.tdur
+        
+        self.sample_interval = self.rate_sec[0] # express in seconds
+        self.traces = np.array(self.data)
+        # nchannels = self.data.shape[0]
+        dt = self.sample_interval  # makes assumption that rate is constant in a block
+        if self.NWB:
+            time_ax = 0
+            rec_ax = 1
+        else:
+            time_ax = 1
+            rec_ax = 0
+        self.time_base = self.time[: self.traces.shape[time_ax]]  # in seconds
+        points = self.data.shape[time_ax]
+        recs = range(self.data.shape[rec_ax])
+
+        if self.dmode == "CC":  # use first channel
+            mainch = 0
+            cmdch = 1
+        else:  # assumption is swapped - for this data, that means voltage clamp mode.
+            mainch = 1
+            cmdch = 0
+
+        cmds = self.cmddata  # self.traces[:,cmdch,:]
+        self.cmd_wave = self.cmddata  # np.squeeze(self.traces[:, cmdch, :])
+        diffpts = self.traces.shape[time_ax] - self.cmd_wave.shape[time_ax]
+        ntr = self.cmd_wave.shape[rec_ax]
+        if diffpts > 0:
+            self.cmd_wave = np.pad(
+                self.cmd_wave,
+                pad_width=(0, diffpts),
+                mode="constant",
+                constant_values=0.0,
+            )[
+                :, :ntr
+            ]  # awkward
+        self.holding = np.zeros((ntr, 1))
+        t0 = int(self.tstart / dt)
+        t1 = int(self.tend / dt)
+        if cmds.shape[0] > 1:
+            if self.NWB:
+                self.values = np.nanmean(
+                    self.cmd_wave[t0:t1, :], axis=time_ax
+                )  # express values in amps
+                self.holding = np.nanmean(self.cmd_wave[:t0, :], axis=time_ax)
+                self.cmd_wave = self.cmd_wave.T
+                self.traces = self.traces.T
+
+            else:
+                self.values = np.nanmean(
+                    self.cmd_wave[:, t0:t1], axis=time_ax
+                )  # express values in amps
+                self.holding = np.nanmean(self.cmd_wave[:, :t0], axis=time_ax)
+        else:
+            self.values = np.zeros_like(self.traces.shape[1:2])
+        self.commandLevels = self.values
+        # for i in range(self.traces.shape[0]):
+        #     mpl.plot(self.time, self.traces[i])
+        #     mpl.plot(self.time[:self.cmd_wave[i].shape[0]], self.cmd_wave[i])
+        # mpl.show()
+
+        info = [
+            {"units": "A", "values": self.values, "name": "Command"},  # dict 0
+            {"name": "Time", "units": "s", "values": self.time_base},  # dict 1
+            {
+                "ClampState": {  # note that many of these values are just defaults and cannot be relied upon
+                    "primaryGain": 1.0,
+                    "ClampParams": {
+                        "OutputZeroEnable": 0,
+                        "PipetteOffset": 0.0,
+                        "Holding": self.holding,
+                        "PrimarySignalHPF": 0.0,
+                        "BridgeBalResist": self.bridgeBalance,
+                        "PrimarySignalLPF": self.primarySignalLPF,
+                        "RsCompBandwidth": 0.0,
+                        "WholeCellCompResist": 0.0,
+                        "WholeCellCompEnable": 6004,
+                        "LeakSubResist": 0.0,
+                        "HoldingEnable": 1,
+                        "FastCompTau": 0.0,
+                        "SlowCompCap": 0.0,
+                        "WholeCellCompCap": 0.0,
+                        "LeakSubEnable": 6004,
+                        "NeutralizationCap": self.neutralizingCap,
+                        "BridgeBalEnable": 0,
+                        "RsCompCorrection": 0.0,
+                        "NeutralizationEnable": 1,
+                        "RsCompEnable": 6004,
+                        "OutputZeroAmplitude": 0.0,
+                        "FastCompCap": 0.0,
+                        "SlowCompTau": 0.0,
+                    },
+                    "secondarySignal": "Command Current",
+                    "secondaryGain": 1.0,
+                    "secondaryScaleFactor": 2e-09,
+                    "primarySignal": "Membrane Potential",
+                    "extCmdScale": 4e-10,
+                    "mode": self.dmode,
+                    "holding": self.holding,
+                    "primaryUnits": "V",
+                    "LPFCutoff": 10000.0,
+                    "secondaryUnits": "A",
+                    "primaryScaleFactor": 0.1,
+                    "membraneCapacitance": 0.0,
+                },
+                "Protocol": {
+                    "recordState": True,
+                    "secondary": None,
+                    "primary": None,
+                    "mode": "IC",
+                },
+                "DAQ": {
+                    "command": {
+                        "numPts": points,
+                        "rate": self.sample_interval,
+                        "type": "ao",
+                        "startTime": 0.0,
+                    },
+                    "primary": {
+                        "numPts": points,
+                        "rate": self.sample_interval,
+                        "type": "ai",
+                        "startTime": 0.0,
+                    },
+                    "secondary": {
+                        "numPts": points,
+                        "rate": self.sample_interval,
+                        "type": "ai",
+                        "startTime": 0.0,
+                    },
+                },
+                "startTime": 0.0,
+            },
+        ]
 
         # if self.traces.shape[0] > 1:
         #     # depending on the mode, select which channel goes to traces
@@ -238,17 +311,19 @@ class MakeClamps:
         #     self.traces[0,mainch,:] = self.traces[0,mainch,:]
 
         self.traces = EM.MetaArray(self.traces, info=info)
-        self.sample_rate = np.ones(self.traces.shape[rec_ax])*self.sample_interval
-        self.cmd_wave = EM.MetaArray(self.cmd_wave,
-             info=[{'name': 'Command', 'units': 'A',
-              'values': np.array(self.values)},
-              self.traces.infoCopy('Time'), self.traces.infoCopy(-1)])
-
+        self.sample_rate = np.ones(self.traces.shape[rec_ax]) * self.sample_interval
+        self.cmd_wave = EM.MetaArray(
+            self.cmd_wave,
+            info=[
+                {"name": "Command", "units": "A", "values": np.array(self.values)},
+                self.traces.infoCopy("Time"),
+                self.traces.infoCopy(-1),
+            ],
+        )
 
         self.spikecount = np.zeros(len(recs))
-        self.rgnrmp = [0, 0.004]
+        self.rgnrmp = [0.0, self.tstart - self.sample_interval]
         return True
-    
-    def setProtocol(self, protocol):
-        self.protocol=protocol
 
+    def setProtocol(self, protocol):
+        self.protocol = protocol
