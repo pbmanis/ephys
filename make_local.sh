46e4a085f7132c4221ded8909054a5142af7ef0c
Paul Manis <pmanis@med.unc.edu>
2024-08-08 17:35:09 -0400

    update requirements, go to py 3.12

--- make_local.sh
+++ make_local.sh
@@ -10,12 +10,12 @@ then
 else
     echo "No previous environment - ok to proceed"
 fi
-
-python3.12 -m venv $ENVNAME
+PYTHONVER="python3.11"
+$PYTHONVER -m venv $ENVNAME
 source $ENVNAME/bin/activate
 pip3 install --upgrade pip  # be sure pip is up to date in the new env.
 pip3 install wheel  # seems to be missing (note singular)
-python3.12 -m pip install Cython
+$PYTHONVER -m pip install Cython
 #pip3 install h5py --no-build-isolation
 # brew install hdf5
 # pip3 install h5py --no-build-isolation
