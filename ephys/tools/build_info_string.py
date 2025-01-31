2c3d38258981858e2144523e765eeef83393d0d4
Paul Manis <pmanis@med.unc.edu>
2025-01-31 16:32:46 -0500

    Fix so that all info is displayed, but with a warning about missing fields.

--- ephys/tools/build_info_string.py
+++ ephys/tools/build_info_string.py
@@ -9,19 +9,22 @@ def build_info_string(AR, path_to_cell):
     slice_info = AR.readDirIndex(currdir=Path(AR.protocol).parent.parent)["."]
     cell_info= AR.readDirIndex(currdir=Path(AR.protocol).parent)["."]
     info_keys = list(info.keys())
-    # print("Info: ", info)
-    if "sex" not in info_keys or "age" not in info_keys or "temperature" not in info_keys or "internal" not in info_keys:
-        Logger.critical(f"Missing info in top-level .index file in {path_to_cell}")
-        return("Missing Critical information in .index file")
+    missing = False
 
     slice_info_keys = list(slice_info.keys())
     cell_info_keys = list(cell_info.keys())
     if "animal_identifier" in info_keys:
         infostr += f"ID: {info['animal_identifier']:s}, "
-    # if "sex" in info_keys:
-    #     infostr += f"{info["sex"].upper():s}, "
-    # if "age" in info_keys:
-    #     infostr += f"{info["age"].upper():s}, "
+    if "sex" in info_keys:
+        infostr += f"{info["sex"].upper():s}, "
+    else:
+        infostr += f"sex: ND, "
+        missing = True
+    if "age" in info_keys:
+        infostr += f"P{info["age"].upper():s}D, "
+    else:
+        infostr += f"age: ND, "
+        missing = True
     if "cell_location" in cell_info_keys:
         infostr += f"{cell_info['cell_location']:s}, "
     if "cell_layer" in cell_info_keys:
@@ -38,23 +41,21 @@ def build_info_string(AR, path_to_cell):
     # notes = self.df.at[icell,'notes']
     if "internal" in info_keys:
         infostr += info["internal"] + ", "
+    else:
+        infostr += "Internal: ND, "
+        missing = True
     if "temperature" in info_keys:
         temp = info["temperature"]
         if temp == "room temperature":
             temp = "RT"
-    if "sex" not in info.keys() or "age" not in info.keys() or "temperature" not in info.keys() or "internal" not in info.keys():
-        Logger.critical(f"Missing info in top-level .index file in {path_to_cell}")
-        exit()
-    # better to fail than substitute a value
-    # else:
-    #     temp = "not specified"
-    # if "sex" not in info.keys():
-    #     info["sex"] = "ND"
-    # if "age" not in info.keys():
-    #     info["age"] = "ND"
+    else:
+        missing=True
+        temp = "temp: ND"
+
     infostr += "{0:s}, ".format(temp)
-    infostr += "{0:s}, ".format(info["sex"].upper())
-    infostr += "{0:s}".format(str(info["age"]).upper())
+
+    if missing:
+        infostr += "\n(NOTE: Critical missing fields)"
     return infostr
 
 def main():
