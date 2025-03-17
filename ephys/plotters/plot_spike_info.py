c4dec95e1ac76d0642391af2960c8d702e2050f0
Paul Manis <pmanis@med.unc.edu>
2025-03-17 12:44:41 -0400

    remove unused import

--- ephys/plotters/plot_spike_info.py
+++ ephys/plotters/plot_spike_info.py
@@ -11,7 +11,7 @@ from typing import Optional
 import textwrap
 import dateutil.parser as DUP
 import matplotlib.pyplot as mpl
-import concurrent.futures
+
 import numpy as np
 import pandas as pd
 import pylibrary.plotting.plothelpers as PH
