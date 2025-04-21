c88d61e50fbe654a1760dd6a49b058a5f2f1df94
Paul Manis <pmanis@med.unc.edu>
2025-04-21 15:57:25 -0400

    add coding sheet command, and use a single requirements.txt file

--- make_local.sh
+++ make_local.sh
@@ -16,7 +16,7 @@ source $ENVNAME/bin/activate
 pip3 install --upgrade pip  # be sure pip is up to date in the new env.
 pip3 install wheel  # seems to be missing (note singular)
 
-pip3 install -r requirements.txt
+pip3 install -r "requirements.txt" -e .
 source $ENVNAME/bin/activate
 python3 --version
 
