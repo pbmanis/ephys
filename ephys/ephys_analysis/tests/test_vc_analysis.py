fbbf6d7067abd3fb2010d41eb56ac8e0237b81e3
Paul Manis <pmanis@med.unc.edu>
2025-07-25 15:11:22 -0400

    test routine for vc_analysis

--- ephys/ephys_analysis/tests/test_vc_analysis.py
+++ ephys/ephys_analysis/tests/test_vc_analysis.py
@@ -8,19 +8,21 @@ import pandas as pd
 from ephys.ephys_analysis import vc_analysis
 from ephys.tools import get_configuration
 from ephys.datareaders import acq4_reader
+import pytest
 
-experiment_name = "DCN_IC_inj"
-
-datasets, experiments = get_configuration.get_configuration("./config/experiments.cfg")
-
-experiment = experiments[experiment_name]
-cell = "2025.07.24_000/slice_000/cell_002"
-protocol = "VC_IH_measure_000"
-ds = pd.read_pickle(Path(experiment['rawdatapath'], experiment['directory'], experiment['databasepath'],
-                         experiment['directory'],
-                         experiment['datasummaryFilename']))
-
+@pytest.mark.skip(reason="Not testable in current environment - must be run from an experiment diretory")
 def test_vc():
+    experiment_name = "DCN_IC_inj"
+
+    datasets, experiments = get_configuration.get_configuration("./config/experiments.cfg")
+
+    experiment = experiments[experiment_name]
+    cell = "2025.07.24_000/slice_000/cell_002"
+    protocol = "VC_IH_measure_000"
+    ds = pd.read_pickle(Path(experiment['rawdatapath'], experiment['directory'], experiment['databasepath'],
+                            experiment['directory'],
+                            experiment['datasummaryFilename']))
+
     this_cell = ds[ds['cell_id'] == cell]
     print(this_cell)
     protocol_path = Path(experiment['rawdatapath'], experiment['directory'],
