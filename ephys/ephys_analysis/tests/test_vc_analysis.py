a2b9ab2ea41ac9343cd6d253fd2d981faf56f74f
Paul Manis <pmanis@med.unc.edu>
2025-07-25 15:02:35 -0400

    a little test routine for the basic VC analysis on Ih

---
+++ ephys/ephys_analysis/tests/test_vc_analysis.py
@@ -0,0 +1,43 @@
+""" Test VC analysis routine on a bit of data
+"""
+
+# run from HK_collab to get the parameters.
+
+from pathlib import Path
+import pandas as pd
+from ephys.ephys_analysis import vc_analysis
+from ephys.tools import get_configuration
+from ephys.datareaders import acq4_reader
+
+experiment_name = "DCN_IC_inj"
+
+datasets, experiments = get_configuration.get_configuration("./config/experiments.cfg")
+
+experiment = experiments[experiment_name]
+cell = "2025.07.24_000/slice_000/cell_002"
+protocol = "VC_IH_measure_000"
+ds = pd.read_pickle(Path(experiment['rawdatapath'], experiment['directory'], experiment['databasepath'],
+                         experiment['directory'],
+                         experiment['datasummaryFilename']))
+
+def test_vc():
+    this_cell = ds[ds['cell_id'] == cell]
+    print(this_cell)
+    protocol_path = Path(experiment['rawdatapath'], experiment['directory'],
+                                    cell, protocol)
+    print(protocol_path)
+    print(protocol_path.is_dir())
+    AR = acq4_reader.acq4_reader(pathtoprotocol = protocol_path)
+    VC = vc_analysis.VCAnalysis()
+    VC.configure(
+        datapath=protocol_path,
+        altstruct=AR,
+        file=None,
+        experiment=experiment,
+        reader=AR,
+        plot=True
+    )
+    VC.analyze_vcs()
+
+if __name__ == "__main__":
+    test_vc()
