ee0abbdd3cc46649d32492fd469d00e427bff0b2
pbmanis <pmanis@med.unc.edu>
2025-08-22 18:19:04 -0400

    remove detrend calls that fail

--- ephys/mini_analyses/tests/test_notchfilter.py
+++ ephys/mini_analyses/tests/test_notchfilter.py
@@ -9,7 +9,7 @@ import ephys.mini_analyses.minis_methods as MM
 import meegkit as MEK
 
 plot = False
-def event(rise:float, decay:float, amp:float=-1, dur:float=0.025, rate:float=1e-4):
+def event(rise:float, decay:float=1.0, amp:float=-1, dur:float=0.025, rate:float=1e-4):
     tb = np.arange(0, dur, rate)
     wv = amp*(1.0-np.exp(-tb/rise))*np.exp(-tb/decay)
     return wv
@@ -22,7 +22,7 @@ npts = int(trace_dur/samprate)
 
 
 notch = []
-notch = np.arange(60, 4000, 60)
+notchfreqs = np.arange(60, 4000, 60)
 
 Q = 6.
 MI = MM.MiniAnalyses()
@@ -41,19 +41,19 @@ tb = np.linspace(0, trace_dur, npts)
 # mpl.show()
 
 # #2. test 60 Hz sine wave
-# data = np.cos(tb*(2*np.pi*60))
+data = np.cos(tb*(2*np.pi*60))
 # data, _, _ = MEK.detrend.detrend(data, order=5)
-# winpts = int(0.5/samprate)
-# hwin = int(winpts/2)
-# window = scipy.signal.windows.hann(winpts, 1)
-# twin = np.concatenate((window[:hwin], np.ones(npts-winpts), window[hwin:]))
-# data = data*twin
-
-# datan = MI.NotchData(data=data, notch=notch, notch_Q = Q)
-# datan = datan[hwin:-hwin]
-# tbn = tb[hwin:-hwin]
+winpts = int(0.5/samprate)
+hwin = int(winpts/2)
+window = scipy.signal.windows.hann(winpts, 1)
+twin = np.concatenate((window[:hwin], np.ones(npts-winpts), window[hwin:]))
+data = data*twin
+print(dir(MI))
+datan = MI.NotchFilterData(data=data, notchfreqs=notchfreqs, notchQ=Q)
+datan = datan[hwin:-hwin]
+tbn = tb[hwin:-hwin]
 # datan, _, _ = MEK.detrend.detrend(datan, order=5)
-# print(f"Max difference: {np.max(datan-data[hwin:-hwin]):.6e}")
+print(f"Max difference: {np.max(datan-data[hwin:-hwin]):.6e}")
 # f, ax = mpl.subplots(1,1)
 # mpl.plot(tb, data, 'k-')
 # mpl.plot(tbn, datan, 'r--')
@@ -75,13 +75,17 @@ for te in evts:
     ist = int(te/samprate)
     if ist+iwv < len(data):
         data[ist:ist+iwv] += wv
-data, _, _ = MEK.detrend.detrend(data, order=5)
+# import matplotlib.pyplot as mpl
+# mpl.plot(data)
+# mpl.show()
+# print(data)
+# data, _, _ = MEK.detrend.detrend(data, order=5)
 winpts = int(1/samprate)
 hwin = int(winpts/2)
 window = scipy.signal.windows.hann(winpts, 1)# , at=100)
 twin = np.concatenate((window[:hwin], np.ones(npts-winpts), window[hwin:]))
 data = data*twin
-datan, _, _ = MEK.detrend.detrend(data, order=5)
+# datan, _, _ = MEK.detrend.detrend(data, order=5)
 # MI.filters.Notch_frequencies = notch
 # MI.filters.Notch_Q = Q
 datan = MI.NotchFilterComb(data=data, notchfreqs=notch, notchQ=Q)
