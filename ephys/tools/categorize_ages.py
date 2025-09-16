a9140cc3d73a4aaafed7b2f4b655e1a3875fe30c
Paul Manis <pmanis@med.unc.edu>
2025-09-16 17:37:23 -0400

    additional related functions for age

--- ephys/tools/categorize_ages.py
+++ ephys/tools/categorize_ages.py
@@ -14,6 +14,23 @@ def numeric_age(row):
     row.age = int("".join(filter(str.isdigit, row.age)))
     return float(row.age)
 
+def numeric_age_from_data(age):
+    """numeric_age convert age to numeric value
+
+    Parameters
+    ----------
+    age : str
+
+    Returns
+    -------
+    float
+    """
+    if isinstance(age, float):
+        return age
+    age = int("".join(filter(str.isdigit, age)))
+    return float(age)
+
+
 """ For an apply function in pandas
 age categories needs to be a dictionary like this:
 {"preweaning": [0, 21], "weaning": [21, 42], "postweaning": [42, 1000]}
@@ -33,5 +50,14 @@ def categorize_ages(row, age_categories):
             row.age_category = k
     return row.age_category
 
-
-    
+def get_age_category(age, age_categories):
+    # given an age string, return the category that fits
+    # from the age_categories dictionary
+    n_age = numeric_age_from_data(age)
+    for k in age_categories.keys():
+        if (
+            n_age >= age_categories[k][0]
+            and n_age <= age_categories[k][1]
+        ):
+            return k
+    return "ND"
