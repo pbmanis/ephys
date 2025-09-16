a4699d613dd0d7a4ecffc16a72b88a051fd18a2e
Paul Manis <pmanis@med.unc.edu>
2025-09-16 17:37:53 -0400

    gather protocols together

--- ephys/tools/db_tools.py
+++ ephys/tools/db_tools.py
@@ -68,3 +68,109 @@ def get_pickled_cell_data(df, idx, analyzed_datapath: Union[Path, str] = None):
         dx = pd.read_pickle(fh, compression="gzip")
     return dx
 
+def gather_protocols(
+    experiment: dict,
+    df: pd.DataFrame,
+    cell_id: str,
+    protocols: list,
+    prots: dict,
+    allprots: dict = None,
+    day: str = None,
+):
+    """
+    Gather all the protocols and sort by functions/types
+    First call will likely have allprots = None, to initialize
+    after that will update allprots with new lists of protocols.
+
+    The variable "allprots" is a dictionary that accumulates
+    the specific protocols from this cell according to type.
+    The type is then used to determine what analysis to perform.
+
+    Parameters
+    ----------
+    protocols: list
+        a list of all protocols that are found for this day/slice/cell
+    prots: dict
+        data, slice, cell information
+    allprots: dict
+        dict of all protocols by type in this day/slice/cell
+    day : str
+        str indicating the top level day for this slice/cell
+
+    Returns
+    -------
+    allprots : dict
+        updated copy of allprots.
+    """
+
+    cell_entry = df.loc[(df.cell_id == cell_id)]
+    if cell_entry.empty:
+        print(f"Date not found: {day!s} {cell_id!s}")
+        # for dx in self.df.date.values:
+        #     CP.cprint("r", f"    day: {dx:s}")
+        raise FileNotFoundError(f"Cell: {self.cell_id!s} was not found in database")
+
+    # CP.cprint("c", f"  ... [Analysis:run] Retrieved cell:\n       {cell_id:s}")
+    icell = cell_entry.index
+    if allprots is None:  # Start with the protocol groups in the configuration file
+        protogroups = list(experiment["protocols"].keys())
+        allprots = {k: [] for k in protogroups}
+        # {"maps": [], "stdIVs": [], "CCIV_long": [], "CCIV_posonly": [], "VCIVs": []}
+    else:
+        protogroups = list(experiment["protocols"].keys())
+    prox = sorted(list(set(protocols)))  # remove duplicates and sort alphabetically
+
+    for i, protocol in enumerate(prox):  # construct filenames and sort by analysis types
+        if len(protocol) == 0:
+            continue
+        # if a single protocol name has been selected, then this is the filter
+        # if (
+        #     (protocol is not None)
+        #     and (len(protocol) > 1)
+        #     and (protocol != protocol)
+        # ):
+        #     continue
+        # clean up protocols that have a path ahead of the protocol (can happen when combining datasets in datasummary)
+        protocol = Path(protocol).name
+
+        # construct a path to the protocol, starting with the day
+        # print(prots["date"], prots["slice_slice"], prots["cell_cell"], protocol)
+        if day is None:
+            c = Path(prots["date"].values[0], prots["slice_slice"].values[0], prots["cell_cell"].values[0], protocol)
+        else:
+            c = Path(day, prots.iloc[i]["slice_slice"].values[0], prots.iloc[i]["cell_cell"].values[0], protocol)
+        c_str = str(c)  # make sure it is serializable for later on with JSON.
+        # maps
+        this_protocol = protocol[:-4]
+        for pg in protogroups:
+            pg_prots = experiment["protocols"][pg]
+            if pg_prots is None:
+                continue
+            if this_protocol in pg_prots:
+                allprots[pg].append(c_str)
+
+        # if x.startswith("Map"):
+        #     allprots["maps"].append(c_str)
+        # if x.startswith("VGAT_"):
+        #     allprots["maps"].append(c_str)
+        # if x.startswith(
+        #     "Vc_LED"
+        # ):  # these are treated as maps, even though they are just repeated...
+        #     allprots["maps"].append(c_str)
+        # # Standard IVs (100 msec, devined as above)
+        # for piv in self.stdIVs:
+        #     if x.startswith(piv):
+        #         allprots["stdIVs"].append(c_str)
+        # # Long IVs (0.5 or 1 second)
+        # if x.startswith("CCIV_long"):
+        #     allprots["CCIV_long"].append(c_str)
+        # # positive only ivs:
+        # if x.startswith("CCIV_1nA_Pos"):
+        #     allprots["CCIV_posonly"].append(c_str)
+        # if x.startswith("CCIV_4nA_Pos"):
+        #     allprots["CCIV_posonly"].append(c_str)
+        # # VCIVs
+        # if x.startswith("VCIV"):
+        #     allprots["VCIVs"].append(c_str)
+    print("Gather_protocols: Found these protocols: ", allprots)
+    return allprots
