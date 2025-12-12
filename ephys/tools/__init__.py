45fa8674d91e4c7a28908c38fc71672dbe0e0534
Paul Manis <pmanis@med.unc.edu>
2025-12-12 17:16:11 -0500

    remove imports - not needed

--- ephys/tools/__init__.py
+++ ephys/tools/__init__.py
@@ -1,23 +1,23 @@
 
 # from . import bridge
-from . import cursor_plot
-from . import data_summary
-# from . import fix_objscale
-from . import tools_plot_maps
-from . import miniviewer
-from . import minicalcs
-from . import digital_filters
-from . import check_rs
-from . import boundrect
-from . import display_acq4
-from . import tifffile
-from . import fitting
-from . import utilities
-from . import get_configuration
-from . import exp_estimator_lmfit
-from . import map_cell_types
-from . import mapevent_analyzer
-from . import functions
+# from . import cursor_plot
+# from . import data_summary
+# # from . import fix_objscale
+# from . import tools_plot_maps
+# from . import miniviewer
+# from . import minicalcs
+# from . import digital_filters
+# from . import check_rs
+# from . import boundrect
+# from . import display_acq4
+# from . import tifffile
+# from . import fitting
+# from . import utilities
+# from . import get_configuration
+# from . import exp_estimator_lmfit
+# from . import map_cell_types
+# from . import mapevent_analyzer
+# from . import functions
 
 
 
